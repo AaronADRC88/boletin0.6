0e34e1052084cf0086bbff9fa44a3a8be6aa5b8e
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-10 12:28:24 +0200

    retangulo

---
+++ src/boletin0/pkg6/Boletin06.java
@@ -0,0 +1,14 @@
+/*cons destruc , metodos de acceso*/
+package boletin0.pkg6;
+
+/**
+ *
+ * @author aferreiradominguez
+ */
+public class Boletin06 {
+
+    public static void main(String[] args) {
+    
+    }
+    
+}
---
+++ src/boletin0/pkg6/Rectangulo.java
@@ -0,0 +1,33 @@
+
+package boletin0.pkg6;
+
+public class Rectangulo {
+    public String cor;
+    private double base;
+    private double altura;
+    //constructores
+    public Rectangulo(){
+    
+    }
+    public Rectangulo(String c, double b , double al){
+        cor=c;
+        base=b;
+        altura=al;
+        }
+    public Rectangulo(double base, double altura){
+        this.base=base;
+        this.altura=altura;
+        
+    }
+    //metodos de acceso get&set (cor é public non precisa metodo de acceso
+    public double setBase(){
+        return 10;
+        }
+    public double setaltura(){
+    return 20;
+    }
+    public double getBase(){
+        getvalor;
+        
+    }
+}
