baac01bf81f693ad720dfa3a93b269803de84d8a
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-14 09:26:22 +0200

    retangulo 1+2 cor

--- src/boletin0/pkg6/Boletin06.java
+++ src/boletin0/pkg6/Boletin06.java
@@ -8,7 +8,13 @@ package boletin0.pkg6;
 public class Boletin06 {
 
     public static void main(String[] args) {
-    
+    Rectangulo rectangulo1=new Rectangulo();
+    Rectangulo rectangulo2=new Rectangulo("azul",4,8);
+    //visualizo base e a cor dos dous
+        System.out.println("cor "+rectangulo1.cor+"\n"+"base= "+rectangulo1.getBase());
+        System.out.println("cor "+rectangulo2.cor+"\n"+"base= "+rectangulo2.getBase());
+        System.out.println("area= "+rectangulo2.calcularArea());
+        
     }
     
 }
--- src/boletin0/pkg6/Rectangulo.java
+++ src/boletin0/pkg6/Rectangulo.java
@@ -23,11 +23,25 @@ public class Rectangulo {
     public void setBase(double b){
         base = b;
         }
-    public double setaltura(){
-    return 20;
+    public void setaltura(double al){
+        altura=al;
     }
     public double getBase(){
         return base;
-        
+     }
+    public double getaltura(){
+        return altura;
     }
+    public double calcularArea(double base , double altura){
+        double area = base*altura ;
+        return area;
+    }
+    public double calcularArea(){
+      return base*altura;
+    }
+    public void calcularPerimetro(double base , double altura){
+        double perimetro=(base+altura)*2;
+        System.out.println("perimetro ="+perimetro);
+    }
+    
 }
