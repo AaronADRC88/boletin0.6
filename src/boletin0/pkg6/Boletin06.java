a74fded5c1ac3740ebfd217c078643dccda49e6e
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-14 09:58:36 +0200

    retangulo 1+2 cor

--- src/boletin0/pkg6/Boletin06.java
+++ src/boletin0/pkg6/Boletin06.java
@@ -13,7 +13,15 @@ public class Boletin06 {
     //visualizo base e a cor dos dous
         System.out.println("cor "+rectangulo1.cor+"\n"+"base= "+rectangulo1.getBase());
         System.out.println("cor "+rectangulo2.cor+"\n"+"base= "+rectangulo2.getBase());
-        System.out.println("area= "+rectangulo2.calcularArea());
+        System.out.println("area= "+rectangulo2.calcularArea(rectangulo2.getBase(),rectangulo2.getaltura()));
+        //doulle valor 10 a base do R2
+        rectangulo2.setBase(10);
+        System.out.println("area nova "+rectangulo2.calcularArea());
+        rectangulo2.calcularPerimetro(rectangulo2.getBase(), rectangulo2.getaltura());
+        
+        
+        
+        
         
     }
     
